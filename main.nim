90ecfb9bb809ab83f43d67e35f9e6e99c328a129
CupRuska <msoxrap857@gmail.com>
2025-11-24 00:32:06 +0200

    1.Debug and rename lang
    2.Fix LogError

--- main.nim
+++ main.nim
@@ -1,8 +1,9 @@
-import os, terminal, strutils
+import os, strutils
+import std/terminal
 
-proc logError*(msg: string="500", error = Exception) =
-    styledEcho (fgRed, "[ERROR] " & msg)
-    raise newException(error, msg)
+proc logError*(msg: string="500", errType: typedesc[Exception] = Exception) =
+    styledEcho(fgRed, "[ERROR] " & msg)
+    raise newException(errType, msg)
 
 proc logWarn*(msg: string="500") =
     styledEcho(fgYellow, "[WARN] " & msg)
@@ -16,35 +17,24 @@ proc print*(args: varargs[string]) =
     for a in args:
         stdout.write(a)
     stdout.write("\n")
-    stdout.flushFile(stdout)
+    stdout.flushFile()
 
-# proc input*(args: varargs[string]): string = Future
+proc start*(symbol: char = '*', name: string = "Example"): string =
+    var line = repeat(symbol, name.len)
+    return line & "\n" & name & "\n" & line
 
-proc start*(symbol: string = "*", name: string = "Example"): string =
-    if symbol.len != 1:
-        logError("Only one symbol", ValueError)
-    
-    var full_text = symbol.repeat(name.len) & "\n"
-    full_text &= name & "\n"
-    full_text &= symbol.repeat(name.len)
-    return full_text
-
-proc pause*(lang: string = "en", color: ForegroundColor) =
+proc pause*(lang: string = "en", color: ForegroundColor = fgWhite) =
     case lang.toLower()
-    of "ru":
-        styledEcho(color, "Нажмите Enter...")
-    of "ua":
-        styledEcho(color, "Натисніть Enter...")
-    of "deu":
-        styledEcho(color, "Drücken Sie die Enter...")
-    else: # English
-        styledEcho(color, "Press Enter...")
+    of "ru": styledEcho(color, "Нажмите Enter...")
+    of "ua": styledEcho(color, "Натисніть Enter...")
+    of "du": styledEcho(color, "Drücken Sie die Enter...")
+    else:    styledEcho(color, "Press Enter...")
     discard stdin.readLine()
 
-proc progressBar*(current: int, total: int, width=30) = # \r[#####-----] 
+proc progressBar*(current: int, total: int, width: float=30.0) =
+    if total == 0: return
     let percent = current.float / total.float
     let filled = int(percent * width)
-    stdout.write("\r[" & "#".repeat(filled) & "-".repeat(width - filled) & "] ")
+    stdout.write("\r[" & "#".repeat(filled) & "-".repeat(int(width) - filled) & "] ")
     stdout.write($int(percent * 100) & "%")
-    stdout.flushFile(stdout)
-    
+    stdout.flushFile()
